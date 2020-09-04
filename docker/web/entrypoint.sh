617bc336ff6d7fe797e401fd9d461190cffc8867
Stephen S Kelley <skelley@fabletics.com>
2020-09-04 17:03:51 -0500

    jupyterlab integration

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+cp /conf/supervisor/jupyter.conf /etc/supervisor/conf.d/jupyter.conf
+mkdir /var/log/jupyterlab/
+jupyter notebook --generate-config
+cat /root/.jupyter/jupyter_notebook_config.py /conf/jupyterlab.conf.append > /root/.jupyter/jupyter_notebook_config.py
+/etc/init.d/supervisor start
 cd web
 python manage.py makemigrations
 python manage.py migrate
--- docker/web/jupyterlab.sh
+++ docker/web/jupyterlab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export DJANGO_SETTINGS_MODULE='base.settings.docker'
 jupyter nbextension install --sys-prefix --py vega
-upyter lab
+jupyter lab --allow-root
 
