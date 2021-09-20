349217c73113c1191cf560418e69b067978711c5
Stephen Kelley <stevek@virtustate.com>
2021-09-19 19:34:12 -0500

    spark jupyter password, minio example

---
+++ docker/spark/jupyter_server_config.py
@@ -0,0 +1,4 @@
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8888
+c.NotebookApp.notebook_dir = '/home/jovyan/work'
+c.NotebookApp.password = u'sha1:d863f7e76ff7:e7ce6f335e7969b3a7521f0c208b4f57ad1427bd'
